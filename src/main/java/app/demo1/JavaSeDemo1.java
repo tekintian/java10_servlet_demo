e9079987aee2099742b4919749831c2a2d818d7c
Tekin Tian <tekintian@gmail.com>
2018-07-10 10:40:00 +0800

    add se demo

---
+++ src/main/java/app/demo1/JavaSeDemo1.java
@@ -0,0 +1,13 @@
+package app.demo1;
+
+public class JavaSeDemo1 {
+    public static  void main(String[] args) {
+
+        System.out.println("~2="+(~2));
+        System.out.println("~-5="+(~-5));
+        System.out.println("-3^3="+(-3^3));
+        System.out.println("1&1="+(1&1));
+
+
+    }
+}
